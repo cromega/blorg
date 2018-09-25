f5855b73c2e4b39b5d5eccf983ab07a4ea8cabfa
Bence Monus <crome@moronia.hu>
2018-09-25 20:18:47 +0100

    don't put shit under Jekyll namespace

--- _plugins/posts_builder.rb
+++ _plugins/posts_builder.rb
@@ -1,17 +1,15 @@
 require_relative "../lib/post_builder"
 
-module Jekyll
-  module PostsBuilder
-    def build_posts(posts)
-      posts.map do |post|
-        PostBuilder.new(post).build
-      end
-    end
-
-    def build_post(post)
+module PostsBuilder
+  def build_posts(posts)
+    posts.map do |post|
       PostBuilder.new(post).build
     end
   end
+
+  def build_post(post)
+    PostBuilder.new(post).build
+  end
 end
 
-Liquid::Template.register_filter(Jekyll::PostsBuilder)
+Liquid::Template.register_filter(PostsBuilder)
