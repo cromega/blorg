2b5187156b4ab7c0d48b63d2fd6c816adfad752a
Bence Monus <crome@moronia.hu>
2020-02-24 23:14:08 +0000

    get pipeline to work again

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-fly -t local set-pipeline -p blorg -c ci/pipeline.yml \
-  -v "blorg-deploy-key=$(cat ../sublimia-platform/secrets/deploy_key)" \
-  -v "deployer-private-key=$(cat ../sublimia-platform/secrets/id_rsa)" \
-  -v "dockerhub-password=$(lpass show --password docker.com)"
-
+fly -t devbox set-pipeline -p blorg -c ci/pipeline.yml \
+  -v "dockerhub-password=$(lpass show --password docker.com)" \
+  -v "deploy-key=$(lpass show blorg-deploy-key --field='Private Key')"
