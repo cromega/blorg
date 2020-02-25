b0a59be5ae3e132f39bbf943cff99b6ec310b726
Bence Monus <crome@moronia.hu>
2020-02-25 22:55:02 +0000

    add deployment bits to pipeline

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -2,4 +2,7 @@
 
 fly -t devbox set-pipeline -p blorg -c ci/pipeline.yml \
   -v "dockerhub-password=$(lpass show --password docker.com)" \
-  -v "deploy-key=$(lpass show blorg-deploy-key --field='Private Key')"
+  -v "deploy-key=$(lpass show blorg-deploy-key --field='Private Key')" \
+  -v "ci-access-key-id=$(lpass show ci-user-access-key --field=access-key-id)" \
+  -v "ci-secret-access-key=$(lpass show ci-user-access-key --field=secret-access-key)"
+
