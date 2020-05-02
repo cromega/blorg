9064fac9ac95dda76539de33c1084c14a8826294
Bence Monus <crome@moronia.hu>
2020-05-02 23:15:45 +0200

    ci folder gets picked up by jekyll

--- .ci/set_pipeline.sh
+++ .ci/set_pipeline.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-fly -t devbox set-pipeline -p blorg -c ci/pipeline.yml \
+lpass status || { echo "Log in to lastpass."; exit 1; }
+
+fly -t ci set-pipeline -p blorg -c .ci/pipeline.yml \
   -v "dockerhub-password=$(lpass show --password docker.com)" \
   -v "deploy-key=$(lpass show blorg-deploy-key --field='Private Key')"
 
