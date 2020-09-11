b2de83b90e49eee4942ea19d6041bdb84d53e92a
Bence Monus <crome@moronia.hu>
2020-09-12 00:02:09 +0200

    dont need this stuff

--- .ci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-cd blog
-bundle exec jekyll build
-
-cp -r _site ../build-output/
-cp nginx.conf ../build-output/
-cp Dockerfile ../build-output/
--- .ci/set_pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-lpass status || { echo "Log in to lastpass."; exit 1; }
-
-fly -t ci set-pipeline -p blorg -c .ci/pipeline.yml \
-  -v "dockerhub-password=$(lpass show --password docker.com)" \
-  -v "deploy-key=$(lpass show blorg-deploy-key --field='Private Key')"
-
